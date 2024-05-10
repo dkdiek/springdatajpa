86e737725d8f565722d009a73d41bd202610597e
Cliff <developerlyk@gmail.com>
2024-05-10 20:44:59 +0900

    first commit

---
+++ src/main/java/study/datajpa/controller/HelloController.java
@@ -0,0 +1,14 @@
+package study.datajpa.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * study.datajpa.controller HelloController
+ *
+ * @author : K
+ */
+
+@RestController
+public class HelloController {
+
+}
